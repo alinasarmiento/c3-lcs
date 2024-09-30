badac1d74512014c689477ca08f682f2a28ada97
Grey Sarmiento <alina.t.sarmiento@gmail.com>
2024-09-30 13:47:00 -0400

    bazel build successful

--- systems/controllers/framework/state_vector.cc
+++
@@ -1,6 +0,0 @@
-#include "systems/framework/state_vector.h"
-
-#include "drake/common/default_scalars.h"
-
-DRAKE_DEFINE_CLASS_TEMPLATE_INSTANTIATIONS_ON_DEFAULT_SCALARS(
-    class ::dairlib::systems::StateVector)
--- systems/controllers/osc/relative_translation_tracking_data.h
+++ systems/controllers/osc/relative_translation_tracking_data.h
@@ -1,77 +1,56 @@
-#include "relative_translation_tracking_data.h"
+#pragma once
 
-using Eigen::Isometry3d;
-using Eigen::MatrixXd;
-using Eigen::Quaterniond;
-using Eigen::Vector3d;
-using Eigen::VectorXd;
+#include "options_tracking_data.h"
 
-using std::string;
-using std::vector;
+namespace dairlib {
+namespace systems {
+namespace controllers {
+/// RelativeTranslationTrackingData is used when we want to track a trajectory
+/// of a relative translational position in the task space.
+/// We built this class on top of two other OscTrackingData's. The user have to
+/// provide `to_frame_data` and `from_frame_data`.
+/// The resulting relative pos/vel/accel are
+///    position(to_frame_data) - position(from_frame_data),
+///    velocity(to_frame_data) - velocity(from_frame_data),
+///    acceleration(to_frame_data) - acceleration(from_frame_data).
 
-using drake::multibody::JacobianWrtVariable;
-using drake::multibody::MultibodyPlant;
-using drake::systems::Context;
+/// WARNING: this doesn't work for the rotational position.
 
-namespace dairlib::systems::controllers {
+/// Developer notes: the current implementation is not the cleanest, since we
+/// have to add a PreUpdate() to make this work.
+class RelativeTranslationTrackingData final : public OptionsTrackingData {
+ public:
+  RelativeTranslationTrackingData(
+      const std::string& name, const Eigen::MatrixXd& K_p,
+      const Eigen::MatrixXd& K_d, const Eigen::MatrixXd& W,
+      const drake::multibody::MultibodyPlant<double>& plant_w_spr,
+      const drake::multibody::MultibodyPlant<double>& plant_wo_spr,
+      OptionsTrackingData* to_frame_data, OptionsTrackingData* from_frame_data);
 
-/**** RelativeTranslationTrackingData ****/
-RelativeTranslationTrackingData::RelativeTranslationTrackingData(
-    const std::string& name, const Eigen::MatrixXd& K_p,
-    const Eigen::MatrixXd& K_d, const Eigen::MatrixXd& W,
-    const drake::multibody::MultibodyPlant<double>& plant_w_spr,
-    const drake::multibody::MultibodyPlant<double>& plant_wo_spr,
-    OptionsTrackingData* to_frame_data, OptionsTrackingData* from_frame_data)
-    : OptionsTrackingData(name, kSpaceDim, kSpaceDim, K_p, K_d, W, plant_w_spr,
-                          plant_wo_spr),
-      to_frame_data_(to_frame_data),
-      from_frame_data_(from_frame_data) {
-  auto states1 = to_frame_data->GetActiveStates();
-  auto states2 = from_frame_data->GetActiveStates();
-  active_fsm_states_ = states1;
-  DRAKE_DEMAND(states1 == states2);
-}
+  void Update(const Eigen::VectorXd& x_w_spr,
+              const drake::systems::Context<double>& context_w_spr,
+              const Eigen::VectorXd& x_wo_spr,
+              const drake::systems::Context<double>& context_wo_spr,
+              const drake::trajectories::Trajectory<double>& traj, double t,
+              double t_gait_cycle, int fsm_state,
+              const Eigen::VectorXd& v_proj) final;
 
-void RelativeTranslationTrackingData::Update(
-    const VectorXd& x_w_spr, const Context<double>& context_w_spr,
-    const VectorXd& x_wo_spr, const Context<double>& context_wo_spr,
-    const drake::trajectories::Trajectory<double>& traj, double t,
-    double t_gait_cycle, const int fsm_state, const VectorXd& v_proj) {
-  // Currently there are redundant calculation here. For both to_frame_data_,
-  // and from_frame_data_, we don't nee to run UpdateDesired, UpdateYError,
-  // UpdateYdotError and UpdateYddotCmd inside Update().
-  // TODO: improve this to make it slightly more efficient.
-  to_frame_data_->Update(x_w_spr, context_w_spr, x_wo_spr, context_wo_spr, traj,
-                         t, t_gait_cycle, fsm_state, v_proj);
-  from_frame_data_->Update(x_w_spr, context_w_spr, x_wo_spr, context_wo_spr,
-                           traj, t, t_gait_cycle, fsm_state, v_proj);
-  OptionsTrackingData::Update(x_w_spr, context_w_spr, x_wo_spr, context_wo_spr,
-                              traj, t, t_gait_cycle, fsm_state, v_proj);
-}
+ private:
+  void UpdateY(const Eigen::VectorXd& x_wo_spr,
+               const drake::systems::Context<double>& context_wo_spr) final;
+  void UpdateYdot(const Eigen::VectorXd& x_wo_spr,
+                  const drake::systems::Context<double>& context_wo_spr) final;
+  void UpdateJ(const Eigen::VectorXd& x_wo_spr,
+               const drake::systems::Context<double>& context_wo_spr) final;
+  void UpdateJdotV(const Eigen::VectorXd& x_wo_spr,
+                   const drake::systems::Context<double>& context_wo_spr) final;
 
-void RelativeTranslationTrackingData::UpdateY(
-    const VectorXd& x_w_spr, const Context<double>& context_w_spr) {
-  y_ = to_frame_data_->GetY() - from_frame_data_->GetY();
-}
+  void CheckDerivedOscTrackingData() final;
 
-void RelativeTranslationTrackingData::UpdateYdot(
-    const VectorXd& x_w_spr, const Context<double>& context_w_spr) {
-  ydot_ = to_frame_data_->GetYdot() - from_frame_data_->GetYdot();
-}
+  OptionsTrackingData* to_frame_data_;
+  OptionsTrackingData* from_frame_data_;
+};
 
-void RelativeTranslationTrackingData::UpdateJ(
-    const VectorXd& x_wo_spr, const Context<double>& context_wo_spr) {
-  J_ = to_frame_data_->GetJ() - from_frame_data_->GetJ();
-}
-
-void RelativeTranslationTrackingData::UpdateJdotV(
-    const VectorXd& x_wo_spr, const Context<double>& context_wo_spr) {
-  JdotV_ = to_frame_data_->GetJdotTimesV() - from_frame_data_->GetJdotTimesV();
-}
-
-void RelativeTranslationTrackingData::CheckDerivedOscTrackingData() {
-  to_frame_data_->CheckOscTrackingData();
-  from_frame_data_->CheckOscTrackingData();
-}
-
-}  // namespace dairlib::systems::controllers
+}  // namespace controllers
+}  // namespace systems
+}  // namespace dairlib
